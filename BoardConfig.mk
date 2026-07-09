9380fc1d942dd4b7511b928842949a5d84fd1af0
Ｇａｎｘ <daffaofficial224@gmail.com>
2026-07-09 17:26:32 +0700

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,6 +7,16 @@
 # Inherit from sdm845-common
 include device/xiaomi/sdm845-common/BoardConfigCommon.mk
 
+$(call soong_config_set,lineageVarsPlugin,TARGET_KERNEL_PLATFORM_TARGET,)
+$(call soong_config_set,lineageVarsPlugin,KERNEL_BUILD_OUT_PREFIX,)
+$(call soong_config_set,lineageVarsPlugin,KERNEL_PATH,$(TARGET_KERNEL_SOURCE))
+$(call soong_config_set,lineageVarsPlugin,KERNEL_ARCH,arm64)
+$(call soong_config_set,lineageVarsPlugin,KERNEL_MAKE_CMD,make)
+$(call soong_config_set,lineageVarsPlugin,KERNEL_MAKE_FLAGS,)
+$(call soong_config_set,lineageVarsPlugin,KERNEL_CROSS_COMPILE,CROSS_COMPILE=aarch64-linux-android-)
+$(call soong_config_set,lineageVarsPlugin,TARGET_PREBUILT_KERNEL_HEADERS,)
+$(call soong_config_set,lineageVarsPlugin,TARGET_KERNEL_SOURCE,$(TARGET_KERNEL_SOURCE))
+
 DEVICE_PATH := device/xiaomi/beryllium
 
 # Assert
